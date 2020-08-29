d78e3c35f9d4948c8719e45336337976bfff3d0e
unknown <shubhamjainiitb@gmail.com>
2020-08-29 17:56:41 +0530

    Added report for Q1.

--- assignment-1-transformations/1/code/myMainScript.m
+++ assignment-1-transformations/1/code/myMainScript.m
@@ -1,4 +1,29 @@
 %% Q1
+% We keep the number of colours per intensity to be 200, as specified in
+% the assignment submission instructions.
+%
+% Throughout this question, we have used the following references: 
+%
+% 1) MATLAB documentation: https://in.mathworks.com/help/matlab/examples.html?category=images_images
+%
+% 2) In specific, from MATLAB documentation we referred to various
+% functions such as imagesc/imread and derivates, truesize and other image
+% output controlling functions. We also used standard matlab functions such
+% as imrotate to confirm the proper working of our codes as compared to the
+% standard MATLAB implementations.
+%
+% 3) We were referred to a github repository containing senior's codes to
+% understand how to vectorize implementations instead of using for loops
+% etc. However, this is just a logical understanding of the functions and our
+% code is quite different (we try to implement the logic given in reference
+% number 4 as far as possible). 
+% In specific, we learnt how to use sub2ind and meshgrid functions from
+% https://github.com/PrieureDeSion/digital-image-processing/blob/master/assignment1_GrayTrans/1/code/myBilinearInterpolation.m.
+%
+% 4) The following "Lectures on Image Processing" were helpful for us to understand
+% a practical implementation of the various interpolation functions: 
+% https://ia802707.us.archive.org/23/items/Lectures_on_Image_Processing/EECE_4353_15_Resampling.pdf
+
 %keeping number of colours per intensity to be 200
 myNumOfColors = 200;
 myColorScale = [ [0:1/(myNumOfColors-1):1]' , ...
@@ -12,22 +37,30 @@ circle_concentric_2 = myShrinkImageByFactorD(circle_concentric_1,2);
 circle_concentric_3 = myShrinkImageByFactorD(circle_concentric_1,3);
 
 %plot the images side-by-side
-subplot(1,3,1), imagesc(circle_concentric_1);
+subplot(1,1,1), imagesc(circle_concentric_1);
 daspect ([1 1 1]);
+truesize([300 300]);
 title('Original Image');
 colorbar;
-subplot(1,3,2), imagesc(circle_concentric_2);
+colormap (myColorScale);
+colormap gray;
+%%
+subplot(1,1,1), imagesc(circle_concentric_2);
 daspect ([1 1 1]);
+truesize([300 300]);
 title('Image with d = 2');
 colorbar;
-subplot(1,3,3), imagesc(circle_concentric_3);
+colormap (myColorScale);
+colormap gray;
+%%
+subplot(1,1,1), imagesc(circle_concentric_3);
 daspect ([1 1 1]);
+truesize([300 300]);
 title('Image with d = 3');
 colorbar;
 axis tight;
 colormap (myColorScale);
 colormap gray;
-
 %% Q1b
 bar = imread('../data/barbaraSmall.png');
 bar = double(bar)/255;
@@ -48,12 +81,15 @@ biliterpbar = myBilinearInterpolation(bar, phi_x, phi_y);
 axis tight;
 colormap (myColorScale);
 colormap gray;
-subplot(1,2,1), imagesc(single(bar));
+subplot(1,1,1), imagesc(single(bar));
 daspect ([1 1 1]);
-colorbar
+truesize([300 300]);
 title('Original Image');
-subplot(1,2,2), imagesc(single(biliterpbar));
+colorbar
+%%
+subplot(1,1,1), imagesc(single(biliterpbar));
 daspect ([103/307 103/205 1]);
+truesize([300 300]);
 title('Bilinear Interpolation');
 colorbar
 
@@ -62,11 +98,12 @@ NNiterpbar = myNearestNeighborInterpolation(bar, phi_x, phi_y);
 axis tight;
 colormap (myColorScale);
 colormap gray;
-subplot(1,2,1), imagesc(single(bar));
+subplot(1,1,1), imagesc(single(bar));
 daspect ([1 1 1]);
 colorbar
 title('Original Image');
-subplot(1,2,2), imagesc(single(NNiterpbar)); % phantom is a popular test image
+%%
+subplot(1,1,1), imagesc(single(NNiterpbar)); % phantom is a popular test image
 daspect ([103/307 103/205 1]);
 title('Nearest Neighbor Interpolation');
 colorbar
@@ -76,11 +113,12 @@ bicubic_iterp_bar = myBicubicInterpolation(bar, phi_x, phi_y);
 axis tight;
 colormap (myColorScale);
 colormap gray;
-subplot(1,2,1), imagesc(single(bar));
+subplot(1,1,1), imagesc(single(bar));
 daspect ([1 1 1]);
 title('Original Image');
 colorbar
-subplot(1,2,2), imagesc(single(bicubic_iterp_bar)); % phantom is a popular test image
+%%
+subplot(1,1,1), imagesc(single(bicubic_iterp_bar)); % phantom is a popular test image
 daspect ([103/307 103/205 1]);
 title('Bicubic Interpolation');
 colorbar
@@ -116,34 +154,42 @@ barbara_face_bicubic = myBicubicInterpolation(barbara_face, face_phi_x, face_phi
 axis tight;
 colormap (myColorScale);
 colormap jet;
-subplot(2,2,1), imagesc(single(barbara_face)); % phantom is a popular test image
+subplot(1,1,1), imagesc(single(barbara_face)); % phantom is a popular test image
 daspect ([1 1 1]);
 title('Original Image');
 colorbar
-subplot(2,2,2), imagesc(single(barbara_face_NN));
+%%
+subplot(1,1,1), imagesc(single(barbara_face_NN));
 daspect ([1/2 1/2 1]);
 title('Nearest Neighbor');
 colorbar
-subplot(2,2,3), imagesc(single(barbara_face_bilinear));
+%%
+subplot(1,1,1), imagesc(single(barbara_face_bilinear));
 daspect ([1/2 1/2 1]);
 title('Bilinear');
 colorbar
-subplot(2,2,4), imagesc(single(barbara_face_bicubic));
+%%
+subplot(1,1,1), imagesc(single(barbara_face_bicubic));
 daspect ([1/2 1/2 1]);
 title('Bicubic');
 colorbar
 %% Q1f
+% We pad the image so as to not lose information in the rotation.
 padded_bar = zeros(140,140);
 padded_bar(20:122,20:122) = bar;
-final = myImageRotation(padded_bar, 30);
+%This function takes anti-clockwise rotations as positive.
+final = myImageRotation(padded_bar, -30);
 axis tight;
 colormap (myColorScale);
 colormap gray;
-subplot(1,2,1), imagesc(bar);
+subplot(1,1,1), imagesc(bar);
 daspect ([1 1 1]);
 title('Original Image');
 colorbar
-subplot(1,2,2), imagesc(final);
+%%
+subplot(1,1,1), imagesc(final);
 daspect ([1 1 1]);
+colormap (myColorScale);
+colormap gray;
 title('30 degree rotation');
 colorbar
