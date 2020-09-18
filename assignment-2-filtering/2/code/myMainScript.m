da97667ad2a884a6f58314d476e6ea4bfb0ae7e2
madhur76 <53146843+madhur76@users.noreply.github.com>
2020-09-18 21:51:58 +0530

    Optimised on Barbara, implemented on 3 images

--- assignment-2-filtering/2/code/myMainScript.m
+++ assignment-2-filtering/2/code/myMainScript.m
@@ -8,14 +8,45 @@ im = imstruct.imageOrig;
 spread = range(im,'all');
 noise = (0.05*spread).*randn(size(im,1));
 ns = im + noise;
-cleaned = myBilateralFiltering(ns,10,10);
+cleaned = myBilateralFiltering(ns,1.4,10);
 subplot(1, 3, 1)
 imshow(mat2gray(im))
 subplot(1, 3, 2)
 imshow(mat2gray(ns))
 subplot(1, 3, 3)
 imshow(mat2gray(cleaned))
-%% error calc
+rmsd = sqrt(mean((cleaned-im).^2, 'all'));
+ogrmsd = sqrt(mean((ns-im).^2, 'all'));
+disp(rmsd);
+disp(ogrmsd);
+%% Second Image
+im = imread('../data/grass.png');
+im = double(im)/255;
+imstruct = load('../data/grassNoisy.mat');
+ns = imstruct.imgCorrupt;
+cleaned = myBilateralFiltering(ns,1.4,10);
+subplot(1, 3, 1)
+imshow(mat2gray(im))
+subplot(1, 3, 2)
+imshow(mat2gray(ns))
+subplot(1, 3, 3)
+imshow(mat2gray(cleaned))
+rmsd = sqrt(mean((cleaned-im).^2, 'all'));
+ogrmsd = sqrt(mean((ns-im).^2, 'all'));
+disp(rmsd);
+disp(ogrmsd);
+%% Third Image
+im = imread('../data/honeyCombReal.png');
+im = double(im)/255;
+imstruct = load('../data/honeyCombReal_Noisy.mat');
+ns = imstruct.imgCorrupt;
+cleaned = myBilateralFiltering(ns,1.4,10);
+subplot(1, 3, 1)
+imshow(mat2gray(im))
+subplot(1, 3, 2)
+imshow(mat2gray(ns))
+subplot(1, 3, 3)
+imshow(mat2gray(cleaned))
 rmsd = sqrt(mean((cleaned-im).^2, 'all'));
 ogrmsd = sqrt(mean((ns-im).^2, 'all'));
 disp(rmsd);
