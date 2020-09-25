9c028cbd2551330a6ed1a20fed40239497c2e948
Mithilesh Vaidya <vaidya.mithilesh@gmail.com>
2020-09-25 22:32:25 +0530

    q1 report done

--- assignment-2-filtering/1/code/myMainScript.m
+++ assignment-2-filtering/1/code/myMainScript.m
@@ -14,15 +14,15 @@
 
 % Parameters:
 %%
-% * Standard deviation of Gaussian: 3
-% * Scaling factor: 0.6
+% * Standard deviation of Gaussian: 5
+% * Scaling factor: 0.9
 
 clc
 imstruct = load('../data/lionCrop.mat');
 im = imstruct.imageOrig;
-filter_len = 25;
-sigma = 3;
-scaling = 0.6;
+sigma = 5;
+filter_len = 4*sigma;
+scaling = 0.9;
 [orig, sharp, mask] = myUnsharpMasking(im, filter_len, sigma, scaling);
 f = figure('visible', 'on');
 subplot(1, 2, 1)
@@ -36,20 +36,21 @@ f = figure('visible', 'on');
 subplot(1, 1, 1)
 imshow(mat2gray(imresize(mask, 0.5)))
 title("High-pass version which is added");
-
+%%
+% Most of the percpetible change can be seen in the mane of the lion.
 %% Image 2
 
 % Parameters:
 %%
-% * Standard deviation of Gaussian: 6
-% * Scaling factor: 0.2
+% * Standard deviation of Gaussian: 10
+% * Scaling factor: 0.5
 
 clc
 imstruct = load('../data/superMoonCrop.mat');
 im = imstruct.imageOrig;
-filter_len = 20;
-sigma = 6;
-scaling = 0.2;
+sigma = 10;
+filter_len = sigma*5;
+scaling = 0.5;
 [orig, sharp, mask] = myUnsharpMasking(im, filter_len, sigma, scaling);
 f = figure('visible', 'on');
 subplot(1, 2, 1)
@@ -58,8 +59,10 @@ title("Original Image");
 subplot(1, 2, 2)
 imshow(mat2gray(sharp))
 title("Sharp Image");
-%%
+
 f = figure('visible', 'on');
 subplot(1, 1, 1)
 imshow(mat2gray(imresize(mask, 0.5)))
 title("High-pass version which is added");
+%%
+% The main difference can be seen in the cluster on the left.
