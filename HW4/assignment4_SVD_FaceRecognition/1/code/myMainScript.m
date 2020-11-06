32663edec57258d720051a4579bd9fcda1b0ca80
Mithilesh Vaidya <vaidya.mithilesh@gmail.com>
2020-11-06 19:44:43 +0530

    q6 finallllllllllly done

--- HW4/assignment4_SVD_FaceRecognition/1/code/myMainScript.m
+++ HW4/assignment4_SVD_FaceRecognition/1/code/myMainScript.m
@@ -1,6 +1,5 @@
 % References:
 % 1. https://in.mathworks.com/help/matlab/ref/eig.html
-% 2. 
 
 clear
 clc
@@ -8,7 +7,6 @@ clc
 r = randi([1 100],2,1);
 A = rand(r(1), r(2));
 % A = eye(3);
-
 
 % get result
 [U, S, V] = mySVD(A);
