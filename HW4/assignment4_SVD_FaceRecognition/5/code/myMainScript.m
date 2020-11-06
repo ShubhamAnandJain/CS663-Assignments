3adb2627ee42b819f667b65c263b358231ecfbff
madhur76 <53146843+madhur76@users.noreply.github.com>
2020-11-06 11:56:41 +0530

    Question 5 and its report completed

--- HW4/assignment4_SVD_FaceRecognition/5/code/myMainScript.m
+++ HW4/assignment4_SVD_FaceRecognition/5/code/myMainScript.m
@@ -1,24 +1,36 @@
-%% MyMainScript
+
 
 tic;
-%% Your code here
+%%
 clear;
 [trainvecspace, testvecspace, trainnumimg, testnumimg, imgrownum, imgcolnum] = orldataloader();
 meanvec = mean(trainvecspace,2);
 trainvecspace = bsxfun(@minus,trainvecspace,meanvec);
 [U,S,V] = svd(trainvecspace,'econ');
-kretain = 20;
-Ured = zeros(size(U));
-Ured(:,1:kretain) = U(:,1:kretain);
-eigenvec = normc(Ured');
-searchspace = eigenvec*trainvecspace;
-oneimg = reshape(testvecspace(:,82), [imgrownum imgcolnum]);
-imshow(uint8(oneimg))
 
+%% Plot eigen vectors
+array = [];
+for val = 1:25
+    array = [array S(val,val)];
+end
+figure
+plot(array)
+title('Top 25 eigenvalues')
+xlabel('Eigen Value number (descending)')
+ylabel('Eigen Value')
+%% Reconstruction of Images
+% for k = [2, 10, 20, 30, 50, 75, 100, 125, 150, 170]
 for k = [2, 10, 20, 30, 50, 75, 100, 125, 150, 170]
     Ured = zeros(size(U));
     Ured(:,1:k) = U(:,1:k);
-    
+    image = Ured*S*V';
+    image = bsxfun(@plus,image,meanvec);
+    oneimg = reshape(image(:,82), [imgrownum imgcolnum]);
+    figure
+    imshow(uint8(oneimg))
+    var = [];
+    var = append(num2str(k),' eigenvalues retained');
+    title(var)
 end
 %%
 toc;
