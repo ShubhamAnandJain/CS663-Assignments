8c51908500dae8e09815a9a4f0047546bc359801
madhur76 <53146843+madhur76@users.noreply.github.com>
2020-11-06 21:30:24 +0530

    corrected Q5 b

--- HW4/assignment4_SVD_FaceRecognition/5/code/myMainScript.m
+++ HW4/assignment4_SVD_FaceRecognition/5/code/myMainScript.m
@@ -8,16 +8,7 @@ meanvec = mean(trainvecspace,2);
 trainvecspace = bsxfun(@minus,trainvecspace,meanvec);
 [U,S,V] = svd(trainvecspace,'econ');
 
-%% Plot eigen vectors
-array = [];
-for val = 1:25
-    array = [array S(val,val)];
-end
-figure
-plot(array)
-title('Top 25 eigenvalues')
-xlabel('Eigen Value number (descending)')
-ylabel('Eigen Value')
+
 %% Reconstruction of Images
 % for k = [2, 10, 20, 30, 50, 75, 100, 125, 150, 170]
 for k = [2, 10, 20, 30, 50, 75, 100, 125, 150, 170]
@@ -30,6 +21,20 @@ for k = [2, 10, 20, 30, 50, 75, 100, 125, 150, 170]
     imshow(uint8(oneimg))
     var = [];
     var = append(num2str(k),' eigenvalues retained');
+    title(var)
+end
+%% Plot eigen vectors
+% The plots below represent normalized eigen vectors generated from the ORL
+% dataset
+for val = 1:25
+    img = reshape(U(:,val), [imgrownum imgcolnum]);
+    img = rescale(img ,0, 255);
+    img=uint8(img);
+    figure;
+    imshow(img)
+    %subplot(5,5,val), imshow(img)
+    var = [];
+    var = append(num2str(val),'th Eigen Vector');
     title(var)
 end
 %%
