5df95ea9cc7137561564d3d73819b0f99ac7f0c6
Mithilesh Vaidya <vaidya.mithilesh@gmail.com>
2020-11-06 20:09:57 +0530

    addedreport for Q1

--- HW4/assignment4_SVD_FaceRecognition/1/code/myMainScript.m
+++ HW4/assignment4_SVD_FaceRecognition/1/code/myMainScript.m
@@ -1,6 +1,13 @@
-% References:
-% 1. https://in.mathworks.com/help/matlab/ref/eig.html
-
+%% Q1: SVD
+%%
+% * To test our code, we generate random matrices with random dimensions (m,n) in
+% the range [1,100].
+% * To test the correctness, we calculate the Frobenius norm of $A -
+% reconstructed$ where A is the original input matrix to the SVD function
+% and reconstructed = $USV^T$ where U, S and V are calculated by the SVD
+% function.
+% * The norm has an order of $10^{-15}$. It is not exactly 0 due to
+% floating point precision limitations.
 clear
 clc
 % input matrix
@@ -14,3 +21,6 @@ A = rand(r(1), r(2));
 % check
 reconstructed = U*S*V';
 norm(A-reconstructed)
+
+% References:
+% 1. https://in.mathworks.com/help/matlab/ref/eig.html
