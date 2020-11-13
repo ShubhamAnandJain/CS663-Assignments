80bde50781b3f31c22ef0fc013cd561a2278416a
Mithilesh Vaidya <vaidya.mithilesh@gmail.com>
2020-11-13 15:31:12 +0530

    q3 hw5

---
+++ HW5/assignment5_DFT/3/code/myNotchFilter.m
@@ -0,0 +1,17 @@
+function [filtered] = myNotchFilter(fourier, u, v)
+filtered = fourier;
+w = int16(3);
+total = 2*w+1;
+notch = ones(total,total);
+for i = 1:w
+    notch(1+i:total-1, 1+i:total-1) = 1e-3*notch(1+i:total-1, 1+i:total-1);
+end
+
+cx = int16(size(fourier, 1)/2)+1;
+cy = int16(size(fourier, 2)/2)+1;
+
+disp_u = u - cx;
+disp_v = v - cy;
+
+filtered(cx+disp_u-w:cx+disp_u+w, cy+disp_v-w:cy+disp_v+w) = filtered(cx+disp_u-w:cx+disp_u+w, cy+disp_v-w:cy+disp_v+w).*notch;
+filtered(cx-disp_u-w:cx-disp_u+w, cy-disp_v-w:cy-disp_v+w) = filtered(cx-disp_u-w:cx-disp_u+w, cy-disp_v-w:cy-disp_v+w).*notch;
